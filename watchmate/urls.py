2dca2567ee6384b7127ddcc8d9afda9c9f83667d
Manjunadh <manjunadh967@gmail.com>
2026-04-22 13:24:47 +0530

    Added home page

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("IMDB API is running 🚀" \
+                        "Please Use any below paths for data:" \
+                        "watch/"
+                        "account/")
+
 
 from django.contrib import admin
 from django.urls import path, include
@@ -8,4 +16,6 @@ urlpatterns = [
     path('watch/', include('watchlist_app.api.urls')),
     # path('api-auth/', include('rest_framework.urls')),
     path('account/', include('user_app.api.urls')),
+    path('', home),
+
 ]
